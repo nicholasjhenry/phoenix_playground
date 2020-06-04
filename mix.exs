3d78a2cd4d7cc6c01a787aad8c4058e5ad1858f0
Nicholas Henry <nicholas.henry@gmail.com>
2020-06-04 19:31:27 -0400

    Configure torch
    https://github.com/mojotech/torch

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule PhoenixGenerated.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:torch, "~> 3.3"}
     ]
   end
 
