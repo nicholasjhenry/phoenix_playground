5a57c482694ce997578d91d836cf464cd93e252b
Nicholas Henry <nicholas.henry@gmail.com>
2020-12-15 09:49:21 -0500

    mix phx.gen.html Accounts User users name:string age:integer

--- lib/phoenix_generated_web/router.ex
+++ lib/phoenix_generated_web/router.ex
@@ -37,6 +37,7 @@ defmodule PhoenixGeneratedWeb.Router do
     scope "/" do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: PhoenixGeneratedWeb.Telemetry
+      resources "/users", UserController
     end
   end
 end
