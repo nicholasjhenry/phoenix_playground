182388471e1db645e93e493f8197bbed7f02b17d
Nicholas Henry <nicholas.henry@gmail.com>
2024-04-30 16:35:08 -0400

    mix phx.new phoenix_generated

---
+++ lib/phoenix_generated_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule PhoenixGeneratedWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use PhoenixGeneratedWeb, :controller` and
+  `use PhoenixGeneratedWeb, :live_view`.
+  """
+  use PhoenixGeneratedWeb, :html
+
+  embed_templates "layouts/*"
+end
