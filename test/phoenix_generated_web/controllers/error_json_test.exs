29fdbb6e8cb2c9a36e2fd106deb2865170440fca
Nicholas Henry <nicholas.henry@gmail.com>
2024-08-30 17:19:16 -0400

    mix phx.new phoenix_generated

---
+++ test/phoenix_generated_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule PhoenixGeneratedWeb.ErrorJSONTest do
+  use PhoenixGeneratedWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert PhoenixGeneratedWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert PhoenixGeneratedWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
---
+++ test/phoenix_generated_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixGeneratedWeb.PageControllerTest do
+  use PhoenixGeneratedWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
